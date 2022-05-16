892f309cdc94a97c24f3c43d711e483dcf28f509
Manassarn Manoonchai <manassarn@gmail.com>
2022-05-16 23:46:53 +0700

    Add Kanit font

--- src/CreditRoll.tsx
+++ src/CreditRoll.tsx
@@ -1,5 +1,4 @@
 import React, {
-	MutableRefObject,
 	RefObject,
 	useCallback,
 	useEffect,
@@ -15,6 +14,7 @@ import {
 	useVideoConfig,
 } from 'remotion';
 import Papa from 'papaparse';
+import './fonts.css';
 
 // Const t2members = fs.openSync(path.join(__dirname, '../ts2members.csv'), 'r');
 const csvPath = staticFile(`/t2members.csv`);
@@ -45,15 +45,19 @@ const t3members = [
 ];
 
 const Members = React.memo(
-	(props: {divRef: RefObject<HTMLDivElement>; translateY: number}) => {
+	(props: {
+		divRef: RefObject<HTMLDivElement>;
+		translateY: number;
+		onLoad: () => void;
+	}) => {
 		const [t2members, setT2Members] = useState<Array<any>>([]);
 		const [handle] = useState(() => delayRender());
 
 		const fetchData = useCallback(async () => {
 			setT2Members(await t2ParsePromise);
-
+			props.onLoad();
 			continueRender(handle);
-		}, [handle]);
+		}, [handle, props]);
 
 		useEffect(() => {
 			fetchData();
@@ -68,48 +72,78 @@ const Members = React.memo(
 					position: 'absolute',
 					textAlign: 'center',
 					transform: `translateY(${props.translateY}px)`,
+					display: 'flex',
+					flexDirection: 'column',
+					gap: '10rem',
 				}}
 			>
-				<h1>ขอขอบคุณเหล่านายทุน</h1>
+				<h1 style={{fontStyle: 'italic', fontWeight: 100}}>
+					ขอขอบคุณเหล่านายทุน
+				</h1>
 
-				<h2>ประธานบอร์ดบริหาร</h2>
+				<section>
+					<h2 style={{fontWeight: 100}}>ประธานบอร์ดบริหาร</h2>
 
-				<div style={{fontSize: '180px'}}>becky_style</div>
+					<div
+						style={{fontSize: '160px', fontStyle: 'italic', fontWeight: 'bold'}}
+					>
+						becky_style
+					</div>
+				</section>
 
-				<h2>ทีมช่วยผ่อนบ้าน</h2>
+				<section>
+					<h2 style={{fontWeight: 100}}>ทีมช่วยผ่อนบ้าน</h2>
 
-				{t3members.map((member, idx) => {
-					return (
-						<div key={idx} style={{color: 'white'}}>
-							{member}
-						</div>
-					);
-				})}
+					<div
+						style={{
+							display: 'flex',
+							flexDirection: 'column',
+							flexWrap: 'wrap',
+							justifyItems: 'center',
+							width: '70%',
+							margin: '0 auto',
+							fontSize: '64px',
+						}}
+					>
+						{t3members.map((member, idx) => {
+							return (
+								<div key={idx} style={{color: 'white'}}>
+									{member}
+								</div>
+							);
+						})}
+					</div>
+				</section>
 
-				<h2>ทีมเลี้ยงข้าว</h2>
+				<section>
+					<h2 style={{fontWeight: 100}}>ทีมเลี้ยงข้าว</h2>
 
-				<div
-					style={{
-						display: 'flex',
-						flexWrap: 'wrap',
-						justifyItems: 'center',
-						width: '60%',
-						margin: '0 auto',
-					}}
-				>
-					{t2members.map((member, idx) => {
-						return (
-							<div
-								key={idx}
-								style={{minWidth: '50%', textAlign: 'center', color: 'white'}}
-							>
-								{member.Member}
-							</div>
-						);
-					})}
-				</div>
+					<div
+						style={{
+							display: 'flex',
+							flexWrap: 'wrap',
+							justifyItems: 'center',
+							width: '70%',
+							margin: '0 auto',
+							fontSize: '40px',
+						}}
+					>
+						{t2members.map((member, idx) => {
+							return (
+								<div
+									key={idx}
+									style={{minWidth: '50%', textAlign: 'center', color: 'white'}}
+								>
+									{member.Member}
+								</div>
+							);
+						})}
+					</div>
+				</section>
 
-				<h2>และทีมเลี้ยงกาแฟทุกท่าน</h2>
+				<section>
+					<h2 style={{fontWeight: 100}}>และทีมเลี้ยงกาแฟทุกท่าน</h2>
+				</section>
 			</div>
 		);
 	}
@@ -123,13 +157,15 @@ export const CreditRoll: React.FC = () => {
 	const [height, setHeight] = useState(0);
 	// Const ref = useRef(null)
 
-	useEffect(() => {
+	// useEffect(() => {
+	function onLoad() {
 		if (ref?.current) {
 			console.log({height: ref.current.scrollHeight});
 
 			setHeight(ref.current.scrollHeight);
 		}
-	}, [ref]);
+	}
+	// }, [ref]);
 
 	const translateY = ~~interpolate(
 		frame,
@@ -148,8 +184,9 @@ export const CreditRoll: React.FC = () => {
 	// });
 
 	return (
-		<div style={{fontSize: '2rem'}}>
-			<Members divRef={ref} translateY={translateY} />
+		<div style={{fontSize: '2rem', fontFamily: 'Kanit', fontWeight: 100}}>
+			{/* eslint-disable-next-line react/jsx-no-bind */}
+			<Members divRef={ref} translateY={translateY} onLoad={onLoad} />
 		</div>
 	);
 };
